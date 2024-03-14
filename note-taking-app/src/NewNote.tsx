fbca43a4c44cef8fa92db04c0cdb05a1d9d33160
mfy98 <mfurkanyigit98@gmail.com>
2024-03-14 22:51:42 +0300

    first commit

---
+++ note-taking-app/src/NewNote.tsx
@@ -0,0 +1,11 @@
+import { NoteForm } from "./NoteForm"
+
+export function NewNote(){
+    return (
+        <>
+            <h1 className="mb-4">This is new note!</h1>
+            <NoteForm />
+        </>
+
+    )
+}
